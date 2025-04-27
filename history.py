ee0ba32a18201ae005443ec145c2f20d5d8a05a8
guicondee <guicondee@outlook.com>
2025-04-27 09:31:42 -0300

    Criada classe Account responsável pelo objto da conta bacaria e seus methods

--- history.py
+++ history.py
@@ -40,6 +40,13 @@ class Client:
     def add_account(self, account):
         self.accounts.append(account)
     
+class Person(Client):
+    def __init__(self, name, date_of_birth, cpf, address):
+        super().__init__(address)
+        self.name = name
+        self.date_of_birth = date_of_birth
+        self.cpf = cpf
+
 class History:
     def __init__(self):
         self._trasactions = []
@@ -65,3 +72,65 @@ class History:
     # TODO: filtrar todas as transações realizadas no dia
     def current_transaction_of_the_day(self):
         pass
+class Account:
+    def __init__(self, number, client):
+        self._balance = 0
+        self._number = number
+        self._agency = "0001"
+        self._client = client
+        self._history = History()
+
+       
+    @classmethod
+    def new_account(cls, number, client):
+        return cls(number, client)
+    
+    @property
+    def balance(self):
+        return self._balance
+    
+    @property
+    def number(self):
+        return self._number
+    
+    
+    @property
+    def agency(self):
+        return self._agency
+    
+    
+    @property
+    def client(self):
+        return self._client
+    
+    @property
+    def history(self):
+        return self._history
+    
+    def withdraw(self, value):
+        balance = self._balance
+        exceeded_balance = value > balance
+
+        if exceeded_balance:
+            print("\n@@@ Operação falhou! Você não tem s aldo suficiente, @@@")
+
+        elif value > 0:
+            self._balance -= value
+            print("\n=== Saque realizado com sucesso! ===")
+            return True
+        
+        else:
+            print("\n@@@ Operação falhou! O valor informado é inválido. @@@")
+
+        return False
+        
+    
+    def deposit(self, value):
+        if value > 0:
+            self._balance += value
+            print("\n=== Depósito realizado com sucesso! ===")
+        else:
+            print("\n@@@ Operação falhou! O valor informado é inválido. @@@")
+            return False
+        
+        return True
